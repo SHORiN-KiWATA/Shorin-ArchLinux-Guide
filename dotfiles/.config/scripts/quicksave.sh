0d56080e5c162f8e74c68b2e15847c56efff2ba9
wu-qingyang <yang000803@gmail.com>
2026-01-24 21:46:34 +0800

    修复quicksave.sh运行无法正确创建快照

--- dotfiles/.config/scripts/quicksave.sh
+++ dotfiles/.config/scripts/quicksave.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-snapper -c root create --description "quicksave"  --cleanup-algorithm number
-snapper -c root cleanup number
-snapper -c home create --description "quicksave" --cleanup-algorithm number
-snapper -c home cleanup number
-notify-send "Quicksaved."
+# 提权但是使用here-doc，不显示代码内容
+if pkexec bash << 'EOF'; then
+    snapper -c root create --description "quicksave" --cleanup-algorithm number
+    snapper -c root cleanup number
+    snapper -c home create --description "quicksave" --cleanup-algorithm number
+    snapper -c home cleanup number
+EOF
+    notify-send "Quicksaved: Success"
+else
+    notify-send -u critical "Quicksaved: Failed!"
+fi
